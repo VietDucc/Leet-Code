473c4a557054bb9b0be6c6346c77e4fbf433606d
VietDucc <21521959@gm.uit.edu.vn>
2025-11-02 17:45:49 +0700

    Reverse Vowels of a String

---
+++ reverseVowelsOfAString.java
@@ -0,0 +1,47 @@
+/*
+ * Given a string s, reverse only all the vowels in the string and return it.
+
+The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+ 
+
+Example 1:
+
+Input: s = "IceCreAm"
+
+Output: "AceCreIm"
+
+Explanation:
+
+The vowels in s are ['I', 'e', 'e', 'A']. On reversing the vowels, s becomes "AceCreIm".
+
+Example 2:
+
+Input: s = "leetcode"
+
+Output: "leotcede"
+ */
+
+class Solution {
+    public String reverseVowels(String s) {
+        char[] arr = s.toCharArray();
+        int i = 0, j = arr.length - 1;
+
+        while (i < j) {
+            if (vowels.contains(arr[i]) && vowels.contains(arr[j])) {
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            } else if (!vowels.contains(arr[i])) {
+                i++;
+            } else
+                j--;
+        }
+        return new String(arr);
+    }
+
+    private static final Set<Character> vowels = new HashSet<>(Arrays.asList(
+            'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+}
